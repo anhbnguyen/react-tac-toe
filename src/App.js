d898a6a01af7d36265b08bd45f6432b74d98a263
Joey Nguyen <jnn193@gmail.com>
2018-03-04 01:43:13 -0600

    Implement HTML and CSS for game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,31 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React-Tac-Toe</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="game-message">
+          <h2>Turn: Player 1</h2>
+        </section>
+        <ul id="game">
+          {/* first row */}
+          <li className="o">O</li>
+          <li className="x">X</li>
+          <li></li>
+          {/* second row */}
+          <li></li>
+          <li></li>
+          <li></li>
+          {/* third row */}
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <button className="reset-game">Reset Game</button>
+        <div className="scores">
+            <h2>Score Card</h2>
+            Player 1: <span id="player-one-score">0</span> <br />
+            Player 2: <span id="player-two-score">0</span> <br />
+        </div>
       </div>
     );
   }
