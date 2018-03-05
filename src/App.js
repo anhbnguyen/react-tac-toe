e1570c1b629da238ce334c7bfd42aa2f5620ed98
Joey Nguyen <jnn193@gmail.com>
2018-03-05 16:26:30 -0600

    Moved initialState and winningCombinations into constants.js file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
 import React, { Component } from 'react';
+import { initialState, winningCombinations } from './constants.js'
 import './App.css';
-
-const initialState = {
-  squares: {
-    1: { content: '', played: false },
-    2: { content: '', played: false },
-    3: { content: '', played: false },
-    4: { content: '', played: false },
-    5: { content: '', played: false },
-    6: { content: '', played: false },
-    7: { content: '', played: false },
-    8: { content: '', played: false },
-    9: { content: '', played: false },
-  },
-  gameStatus: 'play',
-  playerOneTurn: true,
-  playerOneSquares: [],
-  playerTwoSquares: [],
-  playerOneScore: 0,
-  playerTwoScore: 0,
-}
-
-const winningCombinations = [
-  ['1', '2', '3'],
-  ['1', '4', '7'],
-  ['1', '5', '9'],
-  ['2', '5', '8'],
-  ['3', '6', '9'],
-  ['3', '5', '7'],
-  ['4', '5', '6'],
-  ['7', '8', '9'],
-];
 
 class App extends Component {
   state = initialState
---
+++ src/constants.js
@@ -0,0 +1,30 @@
+export const initialState = {
+  squares: {
+    1: { content: '', played: false },
+    2: { content: '', played: false },
+    3: { content: '', played: false },
+    4: { content: '', played: false },
+    5: { content: '', played: false },
+    6: { content: '', played: false },
+    7: { content: '', played: false },
+    8: { content: '', played: false },
+    9: { content: '', played: false },
+  },
+  gameStatus: 'play',
+  playerOneTurn: true,
+  playerOneSquares: [],
+  playerTwoSquares: [],
+  playerOneScore: 0,
+  playerTwoScore: 0,
+};
+
+export const winningCombinations = [
+  ['1', '2', '3'],
+  ['1', '4', '7'],
+  ['1', '5', '9'],
+  ['2', '5', '8'],
+  ['3', '6', '9'],
+  ['3', '5', '7'],
+  ['4', '5', '6'],
+  ['7', '8', '9'],
+];
