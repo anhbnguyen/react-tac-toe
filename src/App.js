d9332fe3d0e236b769ed46b06b996a27b9fbce48
Joey Nguyen <jnn193@gmail.com>
2018-03-05 23:15:34 -0600

    Implement design from codepen.io/roykatsiri/pen/vbegh into React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+
+                <header className="app-header">
+                    <h1 className="app-title">React Tac Toe</h1>
+                </header>
+
+                <section className="game-message">
+                    <h2>Turn: Player 1</h2>
+                </section>
+
+                <ul className="game">
+                    {/* first row */}
+                    <li className="o"></li>
+                    <li className="x"></li>
+                    <li></li>
+                    {/* second row */}
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    {/* third row */}
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <button className="reset-game">Reset Game</button>
+
+                <div className="scores">
+                    <h2>Score Card</h2>
+                    <p>Player 1: 0</p>
+                    <p>Player 2: 0</p>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
